981cd0309edc16c4c3c9ac6c02fcf9f22359611a
Ruggles-Jennifer <43383868+JeneralLee@users.noreply.github.com>
2018-11-25 23:17:14 -0600

    touch event added

--- scripts/cssanimations.js
+++ scripts/cssanimations.js
@@ -1,5 +1,5 @@
 function myMove() {
-  var elem = document.getElementById("profile-image");   
+  var elem = document.getElementById("myAnimation");   
   var pos = 0;
   var id = setInterval(frame, 10);
   function frame() {
