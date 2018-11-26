ac5d566173a3878571c6b688d51fdafeb357d877
Ruggles-Jennifer <43383868+JeneralLee@users.noreply.github.com>
2018-11-26 00:24:55 -0600

    add transition to header

--- scripts/csstransitions.js
+++ scripts/csstransitions.js
@@ -4,7 +4,13 @@ document.getElementById("header").style.backgroundColor = "navy";
 document.getElementById("p").style.backgroundColor = "navy";
 }
 
-function changeTransition() {
+function changeTransform() {
     document.getElementById("profile-image").style.WebkitTransition = "all 4s";
     document.getElementById("profile-image").style.transition = "all 4s";  
+}
+
+function addTransition() {
+    document.getElementById("header").style.color = "azure";
+    document.getElementById("header").style.backgroundColor = "rgb(85, 153, 113)";
+    document.getElementById("header").style.transition = "all 4s ease";
 }
